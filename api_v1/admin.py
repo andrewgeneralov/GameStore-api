4dfcfb1af0babdf75215f5ecabc54fb2d3d77b45
andrewgeneralov <996lol699@gmail.com>
2026-09-03 22:00:45 +0300

    Регистрация моделей в административной панели Django

--- api_v1/admin.py
+++ api_v1/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Developer, Game, Order
 
-# Register your models here.
+
+@admin.register(Developer)
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'country', 'established_year')
+    search_fields = ('name', 'country')
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'price', 'developer', 'is_available')
+    list_filter = ('genre', 'is_available', 'developer')
+    search_fields = ('title',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer_email', 'game', 'status', 'order_date')
+    list_filter = ('status', 'order_date')
